288bdc1f882e18bf827ba32a8315c2b6d9066682
deadwizard21 <54216300+deadwizard21@users.noreply.github.com>
2019-09-02 21:24:04 -0500

    updated app.js file
    contains new DOM  Access & manipulation

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,22 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+scores =[0,0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random()*6)+1;
+
+//setter
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>'+ dice + '</em>';
+
+//getter
+//id #
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+// class .
+document.querySelector('.dice').style.display = 'none';
