e320f83960c5f954a464f4265b2732040dd43cfa
deadwizard21 <54216300+deadwizard21@users.noreply.github.com>
2019-09-08 02:49:15 -0500

    Add files via upload

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -10,22 +10,14 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer;
-scores =[0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
 
 
 
 
 // class .
-document.querySelector('.dice').style.display = 'none';
 
-//this only works for IDs
-document.getElementById('score-0').textContent=0;
-document.getElementById('score-1').textContent=0;
-document.getElementById('current-0').textContent=0;
-document.getElementById('current-1').textContent=0;
 
 //this is called an atonomous function
 document.querySelector('.btn-roll').addEventListener('click', function(){
@@ -48,26 +40,78 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 	}else{
 		
 		//next player
-		//turnery operator
-		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-		roundScore = 0;
-		
-		document.getElementById('current-0').textContent = '0';
-		document.getElementById('current-1').textContent = '0';
-		
-		//toggle adds or removes depending if it exists
-		document.querySelector('player-0-panel').classList.toggle('active');
-		document.querySelector('player-1-panel').classList.toggle('active');
-		
-		//document.querySelector('player-0-panel').classList.remove('active');
-		//document.querySelector('player-1-panel').classList.add('active');
-		
-		document.querySelector('.class').style.display = 'none';
+		nextPlayer();
 	}
 	
 });
 
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+	//add current score to Global score
+	scores[activePlayer] += roundScore;
+	
+	//update the UI
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+	
+	//check if the player won the game
+	if (scores[activePlayer] >= 10){
+		document.querySelector('#name-'+activePlayer).textContent = 'Winner';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+		document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+	}else{
+		nextPlayer();
+	}
+        
+	
+
+
+	
+});
+
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    //toggle adds or removes depending if it exists
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+};
+
+
+document.querySelector('.btn-new').addEventListener('click',init);
+
+
+function init(){
+	scores =[0,0];
+	roundScore = 0;
+	activePlayer = 0;
+	
+	document.querySelector('.dice').style.display = 'none';
+
+	//this only works for IDs
+	document.getElementById('score-0').textContent=0;
+	document.getElementById('score-1').textContent=0;
+	document.getElementById('current-0').textContent=0;
+	document.getElementById('current-1').textContent=0;
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	
+	document.querySelector('.player-0-panel').classList.add('active');
+};
 
 
 
